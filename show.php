1d30595c7c04d9910ff71dac1724ef03b55bdf30
dfernandezpozo <dfernandezpozo@iessonferrer.net>
2025-10-27 17:30:30 +0100

    añadido método show

---
+++ show.php
@@ -0,0 +1,39 @@
+<?php
+
+require "utilities.php";
+
+function showInfo()
+{
+
+    $datos_json = llamarJson();
+
+    echo ("Mostrando todas las tareas: " . "\n");
+    print_r($datos_json);
+
+    echo "Si pulsa 1 podrá ver las tareas completadas, si pulsa 2 verá las no completadas" . "\n";
+
+    $respuesta1 = (fgets(STDIN));
+
+    if ((int)$respuesta1 === 1) {
+
+        foreach ($datos_json["tasks"] as $index => $tasks) {
+
+            if ($tasks["completed"] === true) {
+                print_r($tasks);
+            }
+        }
+    } elseif ((int)$respuesta1 === 2) {
+        echo "Tareas no completadas:\n";
+        foreach ($datos_json["tasks"] as $task) {
+            if ($task["completed"] === false) {
+                print_r($task);
+                echo "\n";
+            }
+        }
+    }
+}
+
+
+
+
+showInfo();
